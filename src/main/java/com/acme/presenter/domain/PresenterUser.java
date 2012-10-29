4a520e1629682a7ca452b8918bb0b0e81d3201b3
doktoric <doktoricsrichard@gmail.com>
2012-10-29 15:46:03 +0100

    database properties setup

--- src/main/java/com/acme/presenter/domain/PresenterUser.java
+++ src/main/java/com/acme/presenter/domain/PresenterUser.java
@@ -3,10 +3,12 @@ package com.acme.presenter.domain;
 import java.util.Calendar;
 import java.util.HashSet;
 import java.util.Set;
+
 import javax.persistence.ElementCollection;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.mail.MailSender;
@@ -14,13 +16,15 @@ import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
 
+import com.acme.presenter.domain.Adress;
+import com.acme.presenter.domain.Roles;
+
 @RooJavaBean
 @RooToString
 @RooJpaActiveRecord
 public class PresenterUser {
 
-    @Autowired
-    private transient MailSender mailTemplate;
+  
 
     private String firstName;
 
